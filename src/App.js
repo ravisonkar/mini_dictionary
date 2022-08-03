e28bda2094ab1986e158dbdb785933ef0c452242
ravi.sonkar <ravi.sonkar@VLINK.IN>
2022-08-03 12:50:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      search_word: '',
+      search_word_definitions: [],
+      loader_visible: false,
+      word_not_found_message: '',
+      word_not_found_message_visible: false
+    }
+  }
+
+  inputWordHandler = (event) => {
+    this.setState({
+      search_word: event.target.value
+    })
+
+  }
+
+  submitHandler = (e) => {
+    try {
+      if (this.state.search_word.length > 1) {
+        axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.state.search_word}`).then((res) => {
+          if (res.status == 200) {
+
+            this.setState({
+              loader_visible: true,
+              word_not_found_message_visible: false
+            })
+
+            var definition_data = []
+
+            res.data[0].meanings.map((definitions) => (
+              definitions.definitions.map(async (definition) => (
+                await definition_data.push(definition)
+              ))
+            ))
+
+            this.setState({
+              search_word_definitions: definition_data,
+              loader_visible: false
+            })
+
+          } else {
+            this.setState({
+              word_not_found_message: "Sorry pal, we couldn't find definitions for the word you were looking for.",
+              word_not_found_message_visible: true
+            })
+          }
+        })
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    return (
+        <div style={{ border: '1px solid black', margin: 'auto', maxWidth: window.innerWidth < 600 ?'100%' : '50%',height:'90vh',overFlow:'auto'}}>
+          <div className='d-flex justify-content-center'>
+            <form>
+              <h1 className='mt-3 text-center border-bottom'>Mini Dictionary</h1>
+              <div className="form-group d-flex mt-5">
+                <input type="text" className="form-control" placeholder="Search word" onChange={this.inputWordHandler} required />
+                <button type="button" className="btn btn-primary mx-1" onClick={this.submitHandler}>Submit</button>
+              </div>
+            </form>
+          </div>
+
+          <div className='d-flex justify-content-center mt-3'> {this.state.search_word_definitions.length > 1 ? (<h5>Definitions</h5>) : ""}</div>
+
+          <div className='d-flex justify-content-center mt-3'>
+            <ul className={this.state.search_word_definitions.length > 4 ? 'overflow px-5' : ''}>
+              {this.state.search_word_definitions.map((definitions) => (
+                <li className='p-3 border bg-light'>{definitions.definition}</li>
+              ))}
+            </ul>
+          </div>
+
+          {this.state.loader_visible == true ? (
+            <div class="d-flex justify-content-center">
+              <div class="spinner-border" role="status">
+              </div>
+            </div>
+          ) : ""}
+
+          <div className='d-flex justify-content-center'>
+            {this.state.word_not_found_message_visible == false ? (
+              <div>{this.state.word_not_found_message}</div>
+            ) : ""}
+          </div>
+
+        </div>
+    )
+  }
 }
-
-export default App;
+export default App
